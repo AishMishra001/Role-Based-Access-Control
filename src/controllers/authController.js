ae7ae373763f83259605a1f6fd9378d0ebae41c4
AishMishra001 <aishlunatic001@gmail.com>
2024-11-25 19:59:52 +0530

    register completed for now

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,28 @@
-
+const bcrypt = require("bcryptjs") ;
+const User = require("../models/userSchema") ; 
+const JWT = require("jsonwebtoken")  ; 
 
 const register = async(req,res)=>{
-  const {username , password , role } = req.body ; 
+  try {
 
-  // btw I can also do this : 
-  // const username = req.body.username  
-  // but I have done object destructing above 
+    const {username , password , role } = req.body ; 
+  
+    // btw I can also do this : 
+    // const username = req.body.username  
+    // but I have done object destructing above 
+  
+    const hashedPassword = bcrypt.hash(password , 10 ) ; 
+  
+    const newUser = new User({ username , password : hashedPassword , role }) ; 
+    await newUser.save() ; 
+    res.status(201).json({
+      message : `User registered with username ${username}`
+    })
+  }catch{
+    res.status(500).json({
+      message : "Something went wrong" 
+    })
+  }
 }
 
 const login = ()=>{
---
+++ src/models/userSchema.js
@@ -0,0 +1,26 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username : {
+      type : String , 
+      require : true , 
+      unique : true 
+    } ,
+    password : {
+      type : String , 
+      require : true , 
+    } , 
+    role : {
+      type : String , 
+      required : true , 
+      enum : ["admin" , "manager" , "user"]  , 
+
+    }
+  } , 
+  {
+    timestamps : true 
+  }
+)
+
+module.exports = mongoose.model("User" , userSchema ) ; 
